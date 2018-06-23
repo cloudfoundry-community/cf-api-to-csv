4f102fb150e372ee889d81259920c2ea356ff7d7
Alex <aanelli@starkandwayne.com>
2018-06-23 18:45:12 -0400

    first stages of rewrite using proper api pagination and structs

--- client.go
+++ client.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"crypto/tls"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
+	"time"
 
 	"github.com/cloudfoundry-community/go-cfclient"
 )
@@ -16,16 +21,78 @@ type Client struct {
 	cfClient   *cfclient.Client
 }
 
-func (c *Client) doGetRequest(path string) (*http.Response, error) {
+type space struct {
+	name                      string
+	guid                      string
+	organizationGUID          string
+	apps                      []appsAPIResource
+	associatedAppCreates      []eventsAPIResource
+	associatedAppStarts       []eventsAPIResource
+	associatedAppUpdates      []eventsAPIResource
+	associatedSpaceCreates    []eventsAPIResponse
+	associatedServiceBindings []serviceBindingsAPIResource
+}
+
+type org struct {
+	name                      string
+	guid                      string
+	apps                      []appsAPIResource
+	associatedAppCreates      []eventsAPIResource
+	associatedAppStarts       []eventsAPIResource
+	associatedAppUpdates      []eventsAPIResource
+	associatedSpaceCreates    []eventsAPIResponse
+	associatedServiceBindings []serviceBindingsAPIResource
+}
+
+func (client *Client) setup() error {
+	yamlConfig, err := parseConfig("./config.yaml")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	//fmt.Printf("yaml config parsed: %v \n", *yamlConfig)
+
+	goCFConfig := &cfclient.Config{
+		ApiAddress:        yamlConfig.APIAddress,
+		Username:          yamlConfig.Username,
+		Password:          yamlConfig.Password,
+		SkipSslValidation: true,
+	}
+
+	goCFClient, err := cfclient.NewClient(goCFConfig)
+	if err != nil {
+		fmt.Println("error creating cfclient")
+		return err
+	}
+	token, err := goCFClient.GetToken()
+	if err != nil {
+		fmt.Println("error getting token fron cfclient")
+		return err
+	}
+	tmpURL, err := url.Parse(yamlConfig.APIAddress)
+	if err != nil {
+		fmt.Println("error parsing yaml config api address into URL")
+		return err
+	}
+	client.authToken = token
+	client.apiURL = tmpURL
+	client.httpClient = &http.Client{Transport: &http.Transport{Proxy: http.ProxyFromEnvironment, TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}}
+	client.cfClient = goCFClient
+
+	return nil
+}
+
+func (client *Client) doGetRequest(path string) (*http.Response, error) {
 	//fmt.Println("performing GET Request on path: " + c.apiURL.String() + path)
-	req, err := http.NewRequest("GET", c.apiURL.String()+path, nil)
+	req, err := http.NewRequest("GET", client.apiURL.String()+path, nil)
 	if err != nil {
 		fmt.Println("error forming http GET request")
 		return &http.Response{}, err
 	}
-	req.Header.Add("Authorization", c.authToken)
+	req.Header.Add("Authorization", client.authToken)
 
-	resp, err := c.httpClient.Do(req)
+	resp, err := client.httpClient.Do(req)
 	if err != nil {
 		fmt.Println("error attempting http GET request")
 		return &http.Response{}, err
@@ -35,5 +102,113 @@ func (c *Client) doGetRequest(path string) (*http.Response, error) {
 	// fmt.Println("----------    dump of GET response body    ----------")
 	// fmt.Printf("%s\n", dump)
 	return resp, nil
+}
 
+func (client *Client) getOrgs() ([]org, error) {
+	orgs := []org{}
+	resp, err := client.doGetRequest("/v2/organizations")
+	var in struct {
+		Resources []struct {
+			Metadata struct {
+				GUID string `json:"guid"`
+			} `json:"metadata"`
+			Entity struct {
+				Name string `json:"name"`
+			} `json:"entity"`
+		} `json:"resources"`
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(body, &in)
+	if err != nil {
+		return nil, err
+	}
+
+	for index, resource := range in.Resources {
+		orgs = append(orgs, org{})
+		orgs[index].name = resource.Entity.Name
+		orgs[index].guid = resource.Metadata.GUID
+	}
+	return orgs, nil
+}
+
+func (client *Client) getSpaces() ([]space, error) {
+	spaces := []space{}
+	resp, err := client.doGetRequest("/v2/spaces")
+	var in struct {
+		Resources []struct {
+			Metadata struct {
+				GUID string `json:"guid"`
+			} `json:"metadata"`
+			Entity struct {
+				Name             string `json:"name"`
+				OrganizationGUID string `json:"organization_guid"`
+			} `json:"entity"`
+		} `json:"resources"`
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(body, &in)
+	if err != nil {
+		return nil, err
+	}
+
+	for index, resource := range in.Resources {
+		spaces = append(spaces, space{})
+		spaces[index].name = resource.Entity.Name
+		spaces[index].organizationGUID = resource.Entity.OrganizationGUID
+		spaces[index].guid = resource.Metadata.GUID
+	}
+	return spaces, nil
+}
+
+type cfServiceBindingsAPIResponse struct {
+	TotalResults int         `json:"total_results"`
+	TotalPages   int         `json:"total_pages"`
+	PrevURL      interface{} `json:"prev_url"`
+	NextURL      interface{} `json:"next_url"`
+	Resources    []struct {
+		Metadata struct {
+			GUID      string      `json:"guid"`
+			URL       string      `json:"url"`
+			CreatedAt time.Time   `json:"created_at"`
+			UpdatedAt interface{} `json:"updated_at"`
+		} `json:"metadata"`
+		Entity struct {
+			AppGUID             string `json:"app_guid"`
+			ServiceInstanceGUID string `json:"service_instance_guid"`
+			Credentials         struct {
+				CredsKey100 string `json:"creds-key-100"`
+			} `json:"credentials"`
+			BindingOptions struct {
+			} `json:"binding_options"`
+			GatewayData        interface{} `json:"gateway_data"`
+			GatewayName        string      `json:"gateway_name"`
+			SyslogDrainURL     interface{} `json:"syslog_drain_url"`
+			AppURL             string      `json:"app_url"`
+			ServiceInstanceURL string      `json:"service_instance_url"`
+		} `json:"entity"`
+	} `json:"resources"`
+}
+
+func (client *Client) cfAPIRequest(endpoint string, returnStruct *struct{}) error {
+	resp, err := client.doGetRequest(endpoint)
+	if err != nil {
+		bailWith("err hitting cf events endpoint: %s", err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("error reading resp body")
+		return err
+	}
+	err = json.Unmarshal(body, &returnStruct)
+	if err != nil {
+		fmt.Println("error unmarshalling resp body into json")
+		return err
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -1,26 +1,19 @@
 package main
 
 import (
-	"crypto/tls"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
 	"os"
-	"time"
 
-	"github.com/cloudfoundry-community/go-cfclient"
 	ansi "github.com/jhunt/go-ansi"
 )
 
 func main() {
-	myClient := Client{}
-	err := setup(&myClient)
+	client := Client{}
+	err := client.setup()
 	if err != nil {
 		bailWith("err setting up client: %s", err)
 	}
-	orgs, err := getOrgs(myClient)
+	orgs, err := client.getOrgs()
 	if err != nil {
 		bailWith("error getting orgs: %s", err)
 	}
@@ -30,7 +23,8 @@ func main() {
 
 	//associate app creates with orgs
 	for index, org := range orgs {
-		jsonResponse, err := cfEventsRequest(myClient, "/v2/events?q=type:audit.app.create&q=organization_guid:"+org.guid)
+		response := eventsAPIResponse{}
+		jsonResponse, err := client.cfAPIRequest("/v2/events?q=type:audit.app.create&q=organization_guid:"+org.guid, &response)
 		if err != nil {
 			bailWith("error associating app creates with orgs: %s", err)
 		}
@@ -39,7 +33,7 @@ func main() {
 
 	//associate app starts with orgs
 	for index, org := range orgs {
-		jsonResponse, err := cfEventsRequest(myClient, "/v2/events?q=type:audit.app.start&q=organization_guid:"+org.guid)
+		jsonResponse, err := cfEventsRequest(client, "/v2/events?q=type:audit.app.start&q=organization_guid:"+org.guid)
 		if err != nil {
 			bailWith("error associating app starts with orgs: %s", err)
 		}
@@ -48,7 +42,7 @@ func main() {
 
 	//associate app updates with orgs
 	for index, org := range orgs {
-		jsonResponse, err := cfEventsRequest(myClient, "/v2/events?q=type:audit.app.update&q=organization_guid:"+org.guid)
+		jsonResponse, err := cfEventsRequest(client, "/v2/events?q=type:audit.app.update&q=organization_guid:"+org.guid)
 		if err != nil {
 			bailWith("error associating app updates with orgs: %s", err)
 		}
@@ -57,7 +51,7 @@ func main() {
 
 	//associate space creates with orgs
 	for index, org := range orgs {
-		jsonResponse, err := cfEventsRequest(myClient, "/v2/events?q=type:audit.space.create&q=organization_guid:"+org.guid)
+		jsonResponse, err := cfEventsRequest(client, "/v2/events?q=type:audit.space.create&q=organization_guid:"+org.guid)
 		if err != nil {
 			bailWith("error associating space creates with orgs: %s", err)
 		}
@@ -66,7 +60,7 @@ func main() {
 
 	//get all apps based on org
 	for index, org := range orgs {
-		jsonResponse, err := cfAppsRequest(myClient, "/v2/apps?q=organization_guid:"+org.guid)
+		jsonResponse, err := cfAppsRequest(client, "/v2/apps?q=organization_guid:"+org.guid)
 		if err != nil {
 			bailWith("error associating apps with orgs: %s", err)
 		}
@@ -75,7 +69,7 @@ func main() {
 
 	for index, org := range orgs {
 		for index, app := range orgs[index].apps {
-			jsonResponse, err := cfAppsRequest(myClient, "/v2/service_bindings?q=app_guid:"+orgs[index].apps[index].apps.guid)
+			jsonResponse, err := cfAppsRequest(client, "/v2/service_bindings?q=app_guid:"+orgs[index].apps[index].apps.guid)
 
 		}
 	}
@@ -87,7 +81,7 @@ func main() {
 	fmt.Printf("\n\n\n\n\n")
 
 	//grab all the spaces
-	spaces, err := getSpaces(myClient)
+	spaces, err := getSpaces(client)
 	if err != nil {
 		bailWith("error getting spaces: %s", err)
 	}
@@ -99,7 +93,7 @@ func main() {
 
 	//associate app starts with spaces
 	for index, space := range spaces {
-		jsonResponse, err := cfEventsRequest(myClient, "/v2/events?q=type:audit.app.start&q=space_guid:"+space.guid)
+		jsonResponse, err := cfEventsRequest(client, "/v2/events?q=type:audit.app.start&q=space_guid:"+space.guid)
 		if err != nil {
 			bailWith("error associating app starts with spaces: %s", err)
 		}
@@ -108,7 +102,7 @@ func main() {
 
 	//associate app creates with spaces
 	for index, space := range spaces {
-		jsonResponse, err := cfEventsRequest(myClient, "/v2/events?q=type:audit.app.create&q=space_guid:"+space.guid)
+		jsonResponse, err := cfEventsRequest(client, "/v2/events?q=type:audit.app.create&q=space_guid:"+space.guid)
 		if err != nil {
 			bailWith("error associating app creates with spaces: %s", err)
 		}
@@ -117,7 +111,7 @@ func main() {
 
 	//associate app updates with spaces
 	for index, space := range spaces {
-		jsonResponse, err := cfEventsRequest(myClient, "/v2/events?q=type:audit.app.update&q=space_guid:"+space.guid)
+		jsonResponse, err := cfEventsRequest(client, "/v2/events?q=type:audit.app.update&q=space_guid:"+space.guid)
 		if err != nil {
 			bailWith("error associating app updates with spaces: %s", err)
 		}
@@ -137,357 +131,6 @@ func main() {
 	// for {
 	// 	serve()
 	// }
-}
-
-type space struct {
-	name                      string
-	guid                      string
-	organizationGUID          string
-	associatedAppCreates      cfEventsAPIResponse
-	associatedAppStarts       cfEventsAPIResponse
-	associatedAppUpdates      cfEventsAPIResponse
-	associatedSpaceCreates    cfEventsAPIResponse
-	associatedServiceBindings []*struct{}
-}
-
-func getSpaces(myClient Client) ([]space, error) {
-	spaces := []space{}
-	resp, err := myClient.doGetRequest("/v2/spaces")
-	var in struct {
-		Resources []struct {
-			Metadata struct {
-				GUID string `json:"guid"`
-			} `json:"metadata"`
-			Entity struct {
-				Name             string `json:"name"`
-				OrganizationGUID string `json:"organization_guid"`
-			} `json:"entity"`
-		} `json:"resources"`
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal(body, &in)
-	if err != nil {
-		return nil, err
-	}
-
-	for index, resource := range in.Resources {
-		spaces = append(spaces, space{})
-		spaces[index].name = resource.Entity.Name
-		spaces[index].organizationGUID = resource.Entity.OrganizationGUID
-		spaces[index].guid = resource.Metadata.GUID
-	}
-	return spaces, nil
-}
-
-type org struct {
-	name                      string
-	guid                      string
-	apps                      []Resources
-	associatedAppCreates      cfEventsAPIResponse
-	associatedAppStarts       cfEventsAPIResponse
-	associatedAppUpdates      cfEventsAPIResponse
-	associatedSpaceCreates    cfEventsAPIResponse
-	associatedServiceBindings []*struct{}
-}
-
-func getOrgs(myClient Client) ([]org, error) {
-	orgs := []org{}
-	resp, err := myClient.doGetRequest("/v2/organizations")
-	var in struct {
-		Resources []struct {
-			Metadata struct {
-				GUID string `json:"guid"`
-			} `json:"metadata"`
-			Entity struct {
-				Name string `json:"name"`
-			} `json:"entity"`
-		} `json:"resources"`
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal(body, &in)
-	if err != nil {
-		return nil, err
-	}
-
-	for index, resource := range in.Resources {
-		orgs = append(orgs, org{})
-		orgs[index].name = resource.Entity.Name
-		orgs[index].guid = resource.Metadata.GUID
-	}
-	return orgs, nil
-}
-
-type cfApp struct {
-	Resources []struct {
-		Metadata struct {
-			GUID      string    `json:"guid"`
-			URL       string    `json:"url"`
-			CreatedAt time.Time `json:"created_at"`
-			UpdatedAt time.Time `json:"updated_at"`
-		} `json:"metadata"`
-		Entity struct {
-			Type      string    `json:"type"`
-			Actor     string    `json:"actor"`
-			ActorType string    `json:"actor_type"`
-			ActorName string    `json:"actor_name"`
-			Actee     string    `json:"actee"`
-			ActeeType string    `json:"actee_type"`
-			ActeeName string    `json:"actee_name"`
-			Timestamp time.Time `json:"timestamp"`
-			Metadata  struct {
-				Request struct {
-					Name              string `json:"name,omitempty"`
-					Instances         int    `json:"instances,omitempty"`
-					Memory            int    `json:"memory,omitempty"`
-					State             string `json:"state,omitempty"`
-					EnvironmentJSON   string `json:"environment_json,omitempty"`
-					DockerImage       string `json:"docker_image,omitempty"`
-					DockerCredentials string `json:"docker_credentials,omitempty"`
-				} `json:"request"`
-			} `json:"metadata"`
-			SpaceGUID        string `json:"space_guid"`
-			OrganizationGUID string `json:"organization_guid"`
-		} `json:"entity"`
-	} `json:"resources"`
-}
-type cfEventsAPIResponse struct {
-	apps []cfApp
-}
-
-func cfEventsRequest(myClient Client, endpoint string) (cfEventsAPIResponse, error) {
-	resp, err := myClient.doGetRequest(endpoint)
-	if err != nil {
-		bailWith("err hitting cf events endpoint: %s", err)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("error reading resp body")
-		return cfEventsAPIResponse{}, err
-	}
-	var responseyDoo cfEventsAPIResponse
-	err = json.Unmarshal(body, &responseyDoo)
-	if err != nil {
-		fmt.Println("error unmarshalling resp body into json")
-		return cfEventsAPIResponse{}, err
-	}
-	return responseyDoo, nil
-}
-
-type Resources struct {
-	Metadata struct {
-		GUID      string    `json:"guid"`
-		URL       string    `json:"url"`
-		CreatedAt time.Time `json:"created_at"`
-		UpdatedAt time.Time `json:"updated_at"`
-	} `json:"metadata"`
-	Entity struct {
-		Name                     string      `json:"name"`
-		Production               bool        `json:"production"`
-		SpaceGUID                string      `json:"space_guid"`
-		StackGUID                string      `json:"stack_guid"`
-		Buildpack                interface{} `json:"buildpack"`
-		DetectedBuildpack        interface{} `json:"detected_buildpack"`
-		DetectedBuildpackGUID    interface{} `json:"detected_buildpack_guid"`
-		EnvironmentJSON          interface{} `json:"environment_json"`
-		Memory                   int         `json:"memory"`
-		Instances                int         `json:"instances"`
-		DiskQuota                int         `json:"disk_quota"`
-		State                    string      `json:"state"`
-		Version                  string      `json:"version"`
-		Command                  interface{} `json:"command"`
-		Console                  bool        `json:"console"`
-		Debug                    interface{} `json:"debug"`
-		StagingTaskID            interface{} `json:"staging_task_id"`
-		PackageState             string      `json:"package_state"`
-		HealthCheckHTTPEndpoint  string      `json:"health_check_http_endpoint"`
-		HealthCheckType          string      `json:"health_check_type"`
-		HealthCheckTimeout       interface{} `json:"health_check_timeout"`
-		StagingFailedReason      interface{} `json:"staging_failed_reason"`
-		StagingFailedDescription interface{} `json:"staging_failed_description"`
-		Diego                    bool        `json:"diego"`
-		DockerImage              interface{} `json:"docker_image"`
-		DockerCredentials        struct {
-			Username interface{} `json:"username"`
-			Password interface{} `json:"password"`
-		} `json:"docker_credentials"`
-		PackageUpdatedAt     time.Time   `json:"package_updated_at"`
-		DetectedStartCommand string      `json:"detected_start_command"`
-		EnableSSH            bool        `json:"enable_ssh"`
-		Ports                interface{} `json:"ports"`
-		SpaceURL             string      `json:"space_url"`
-		StackURL             string      `json:"stack_url"`
-		RoutesURL            string      `json:"routes_url"`
-		EventsURL            string      `json:"events_url"`
-		ServiceBindingsURL   string      `json:"service_bindings_url"`
-		RouteMappingsURL     string      `json:"route_mappings_url"`
-	} `json:"entity"`
-}
-type cfAppsAPIResponse struct {
-	Resources []struct {
-		Metadata struct {
-			GUID      string    `json:"guid"`
-			URL       string    `json:"url"`
-			CreatedAt time.Time `json:"created_at"`
-			UpdatedAt time.Time `json:"updated_at"`
-		} `json:"metadata"`
-		Entity struct {
-			Name                     string      `json:"name"`
-			Production               bool        `json:"production"`
-			SpaceGUID                string      `json:"space_guid"`
-			StackGUID                string      `json:"stack_guid"`
-			Buildpack                interface{} `json:"buildpack"`
-			DetectedBuildpack        interface{} `json:"detected_buildpack"`
-			DetectedBuildpackGUID    interface{} `json:"detected_buildpack_guid"`
-			EnvironmentJSON          interface{} `json:"environment_json"`
-			Memory                   int         `json:"memory"`
-			Instances                int         `json:"instances"`
-			DiskQuota                int         `json:"disk_quota"`
-			State                    string      `json:"state"`
-			Version                  string      `json:"version"`
-			Command                  interface{} `json:"command"`
-			Console                  bool        `json:"console"`
-			Debug                    interface{} `json:"debug"`
-			StagingTaskID            interface{} `json:"staging_task_id"`
-			PackageState             string      `json:"package_state"`
-			HealthCheckHTTPEndpoint  string      `json:"health_check_http_endpoint"`
-			HealthCheckType          string      `json:"health_check_type"`
-			HealthCheckTimeout       interface{} `json:"health_check_timeout"`
-			StagingFailedReason      interface{} `json:"staging_failed_reason"`
-			StagingFailedDescription interface{} `json:"staging_failed_description"`
-			Diego                    bool        `json:"diego"`
-			DockerImage              interface{} `json:"docker_image"`
-			DockerCredentials        struct {
-				Username interface{} `json:"username"`
-				Password interface{} `json:"password"`
-			} `json:"docker_credentials"`
-			PackageUpdatedAt     time.Time   `json:"package_updated_at"`
-			DetectedStartCommand string      `json:"detected_start_command"`
-			EnableSSH            bool        `json:"enable_ssh"`
-			Ports                interface{} `json:"ports"`
-			SpaceURL             string      `json:"space_url"`
-			StackURL             string      `json:"stack_url"`
-			RoutesURL            string      `json:"routes_url"`
-			EventsURL            string      `json:"events_url"`
-			ServiceBindingsURL   string      `json:"service_bindings_url"`
-			RouteMappingsURL     string      `json:"route_mappings_url"`
-		} `json:"entity"`
-	} `json:"resources"`
-}
-
-func cfAppsRequest(myClient Client, endpoint string) (cfAppsAPIResponse, error) {
-	resp, err := myClient.doGetRequest(endpoint)
-	if err != nil {
-		bailWith("err hitting cf apps endpoint: %s", err)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("error reading resp body")
-		return cfAppsAPIResponse{}, err
-	}
-	var responseyDoo cfAppsAPIResponse
-	err = json.Unmarshal(body, &responseyDoo)
-	if err != nil {
-		fmt.Println("error unmarshalling resp body into json")
-		return cfAppsAPIResponse{}, err
-	}
-	return responseyDoo, nil
-}
-
-type cfServiceBindingsAPIResponse struct {
-	TotalResults int         `json:"total_results"`
-	TotalPages   int         `json:"total_pages"`
-	PrevURL      interface{} `json:"prev_url"`
-	NextURL      interface{} `json:"next_url"`
-	Resources    []struct {
-		Metadata struct {
-			GUID      string      `json:"guid"`
-			URL       string      `json:"url"`
-			CreatedAt time.Time   `json:"created_at"`
-			UpdatedAt interface{} `json:"updated_at"`
-		} `json:"metadata"`
-		Entity struct {
-			AppGUID             string `json:"app_guid"`
-			ServiceInstanceGUID string `json:"service_instance_guid"`
-			Credentials         struct {
-				CredsKey100 string `json:"creds-key-100"`
-			} `json:"credentials"`
-			BindingOptions struct {
-			} `json:"binding_options"`
-			GatewayData        interface{} `json:"gateway_data"`
-			GatewayName        string      `json:"gateway_name"`
-			SyslogDrainURL     interface{} `json:"syslog_drain_url"`
-			AppURL             string      `json:"app_url"`
-			ServiceInstanceURL string      `json:"service_instance_url"`
-		} `json:"entity"`
-	} `json:"resources"`
-}
-
-func cfServiceBindingsRequest(myClient Client, endpoint string) (cfServiceBindingsAPIResponse, error) {
-	resp, err := myClient.doGetRequest(endpoint)
-	if err != nil {
-		bailWith("err hitting cf apps endpoint: %s", err)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("error reading resp body")
-		return cfServiceBindingsAPIResponse{}, err
-	}
-	var responseyDoo cfServiceBindingsAPIResponse
-	err = json.Unmarshal(body, &responseyDoo)
-	if err != nil {
-		fmt.Println("error unmarshalling resp body into json")
-		return cfServiceBindingsAPIResponse{}, err
-	}
-	return responseyDoo, nil
-}
-
-func setup(myClient *Client) error {
-	yamlConfig, err := parseConfig("./config.yaml")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	//fmt.Printf("yaml config parsed: %v \n", *yamlConfig)
-
-	goCFConfig := &cfclient.Config{
-		ApiAddress:        yamlConfig.APIAddress,
-		Username:          yamlConfig.Username,
-		Password:          yamlConfig.Password,
-		SkipSslValidation: true,
-	}
-
-	goCFClient, err := cfclient.NewClient(goCFConfig)
-	if err != nil {
-		fmt.Println("error creating cfclient")
-		return err
-	}
-	token, err := goCFClient.GetToken()
-	if err != nil {
-		fmt.Println("error getting token fron cfclient")
-		return err
-	}
-	tmpURL, err := url.Parse(yamlConfig.APIAddress)
-	if err != nil {
-		fmt.Println("error parsing yaml config api address into URL")
-		return err
-	}
-	*myClient = Client{
-		authToken:  token,
-		apiURL:     tmpURL,
-		httpClient: &http.Client{Transport: &http.Transport{Proxy: http.ProxyFromEnvironment, TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}},
-		cfClient:   goCFClient,
-	}
-	return nil
 }
 
 func bailWith(f string, a ...interface{}) {
